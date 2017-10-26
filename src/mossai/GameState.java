9315283e6fac6081dec33da9b37b699f95a560a4
AbstractSpaces <dj.bytheriver@gmail.com>
2017-10-26 04:59:09 +0000

    Debugging.

--- src/mossai/GameState.java
+++ src/mossai/GameState.java
@@ -377,6 +377,7 @@ class GameState
             
             Arrays.fill(table, null);
             scores[best]++;
+			turn = best;
             order[0] = best;
             order[1] = (best + 1) % 3;
             order[2]= (best + 2) % 3;
@@ -389,6 +390,7 @@ class GameState
         // Skip the whole process if the node is already expanded.
         if(children == null)
         {
+			System.out.println("Expanding node during agent turn: " + (turn == pos));
             children = new ArrayList();
 
             for(Suit s : Suit.values())
@@ -401,15 +403,21 @@ class GameState
                         // If the agent has this card.
                         if(beliefs[pos].here(Game.intToCard(i)))
                         {
-                                // Simulate a legal move.
-                                GameState child = new GameState(this, false);
-                                child.advance(Game.intToCard(i));
+							// Simulate a legal move.
+							GameState child = new GameState(this, false);
+							System.out.println("Cloned state for child, agent move: " + (child.turn == pos));
+							child.advance(Game.intToCard(i));
+							System.out.println("Agent move made, next turn: " + child.turn + ", is agent: " + (child.turn == pos));
 
-                                // Simulate predicted opponent moves.
-                                while(child.turn != pos)
-                                        child.advance(child.greedyEval());
+							// Simulate predicted opponent moves.
+							while(child.turn != pos)
+							{
+								System.out.println("Simulating turn: " + child.turn + ", is agent: " + (child.turn == pos));
+								child.advance(child.greedyEval());
+								System.out.println("Next turn: " + child.turn + ", is agent: " + (child.turn == pos));
+							}
 
-                                children.add(child);
+							children.add(child);
                         }
                     }
                 }
